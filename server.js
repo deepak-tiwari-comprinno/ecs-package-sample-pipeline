098a9b212f936394e5204db9c2257870d8059575
deepak-tiwari-comprinno <117432042+deepak-tiwari-comprinno@users.noreply.github.com>
2022-12-02 14:10:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:red;">ECS-Package Sample App Deployment v-2</h1> \n');
+  res.send('<h1 style="color:blue;">ECS-Package Sample App Deployment v-2</h1> \n');
 });
 
 app.listen(PORT, HOST);
